e243e13331d0439e1565676096238927fa3fc830
Freddie Coleman <f.coleman@hotmail.co.uk>
2022-06-22 06:50:14 +0100

    extract create coin conditions from inner puzzle + solution

--- service/coin_spend_processor.py
+++ service/coin_spend_processor.py
@@ -2,11 +2,9 @@ import logging
 import time
 
 from chia.types.condition_opcodes import ConditionOpcode
-from chia.util.condition_tools import conditions_by_opcode
+from chia.util.condition_tools import conditions_by_opcode, conditions_dict_for_solution, created_outputs_for_conditions_dict
 from chia.wallet.cat_wallet.cat_utils import match_cat_puzzle
 from clvm.casts import int_from_bytes
-
-from service.utils import parse_sexp_to_conditions
 
 
 class CoinSpendProcessor:
@@ -14,49 +12,44 @@ class CoinSpendProcessor:
     log = logging.getLogger("CoinSpendProcessor")
 
     def process_coin_spends(self, height, header_hash: str, coin_spends, height_persistance):
-        self.log.debug("Processing %i coin spends for block %s at height %i", len(coin_spends), header_hash, height)
+        self.log.info("Processing %i coin spends for block %s at height %i", len(coin_spends), header_hash, height)
 
         for coin_spend in coin_spends:
             outer_puzzle = coin_spend.puzzle_reveal.to_program()
             matched, curried_args = match_cat_puzzle(outer_puzzle)
 
             if matched:
-                _, tail_hash, _ = curried_args
+                _, tail_hash, inner_puzzle = curried_args
 
-                outer_solution = coin_spend.solution.to_program()
                 coin_name = coin_spend.coin.name()
+                outer_solution = coin_spend.solution.to_program()
+                inner_solution = outer_solution.first()
 
-                r = outer_puzzle.run(outer_solution)
-                self.log.info("r=%s", r)
                 self.log.info("outer_solution=%s", outer_solution)
-                _, conditions = parse_sexp_to_conditions(r)
+
+                _, conditions, _ = conditions_dict_for_solution(inner_puzzle, inner_solution, 0)
 
                 if conditions is not None:
-                    cbo = conditions_by_opcode(conditions)
-                    create_coin_conditions = cbo.get(ConditionOpcode.CREATE_COIN)
+                    create_coin_conditions = created_outputs_for_conditions_dict(conditions, coin_name)
                     if create_coin_conditions is not None:
-                        for condition in create_coin_conditions:
-                            puzzle_hash = condition.vars[0]
-                            amount = int_from_bytes(condition.vars[1])
+                        for coin in create_coin_conditions:
+                            # we extract the puzzle hash from the conditions before the CAT is executed
 
-                            if len(condition.vars) < 3:
-                                self.log.warn("Found CAT create coin condition without a hint: %s", condition)
-                            else:
-                                hint = condition.vars[2]
-                                self.record_cat_coin(
-                                    height, coin_name, tail_hash, puzzle_hash, amount, hint
-                                )
+                            self.log.info("inner_puzzle_hash=%s amount=%i", coin.puzzle_hash, coin.amount)
+
+                            self.record_cat_coin(
+                                height, coin_name, tail_hash, coin.puzzle_hash, coin.amount
+                            )
             else:
                 self.log.debug("Found non-CAT coin spend")
 
-    def record_cat_coin(self, height, coin_name, tail_hash, puzzle_hash, amount, hint):
+    def record_cat_coin(self, height, coin_name, tail_hash, inner_puzzle_hash, amount):
         self.log.info(
-            "Recording CAT coin created with coin name %s, TAIL %s, puzzle_hash %s, amount %i, and hint %s, at block height %i",
+            "Recording CAT coin created with coin name %s, TAIL %s, inner_puzzle_hash %s, amount %i, at block height %i",
             coin_name,
             tail_hash,
-            puzzle_hash,
+            inner_puzzle_hash,
             amount,
-            hint,
             height
         )
 
